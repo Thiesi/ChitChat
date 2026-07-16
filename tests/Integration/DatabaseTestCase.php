5b248b8f1d5a4320f5832a87b7c684c56e5bc806
Thiesi <Thiesi@users.noreply.github.com>
2026-07-16 23:36:43 +0200

    Reset room tables in integration tests

--- tests/Integration/DatabaseTestCase.php
+++ tests/Integration/DatabaseTestCase.php
@@ -20,7 +20,7 @@ abstract class DatabaseTestCase extends TestCase
         $this->config = Config::fromEnvironment();
         $this->pdo = Database::connect($this->config);
         $this->pdo->exec(
-            'TRUNCATE TABLE audit_log, user_bans, login_attempts, user_roles, users RESTART IDENTITY CASCADE',
+            'TRUNCATE TABLE room_messages, room_invitations, room_members, rooms, audit_log, user_bans, login_attempts, user_roles, users RESTART IDENTITY CASCADE',
         );
         $this->pdo->exec('UPDATE system_settings SET registration_enabled = TRUE WHERE id = 1');
     }
