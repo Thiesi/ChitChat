d75baa423aa156d526995c48b7db1d81a80e97fb
Thiesi <Thiesi@users.noreply.github.com>
2026-07-16 23:10:23 +0200

    Add session endpoint

---
+++ public/api/v1/session.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use ChitChat\Auth\SessionManager;
+use ChitChat\Auth\UserRepository;
+use ChitChat\Database;
+use ChitChat\Http\ApiResult;
+use ChitChat\Http\Endpoint;
+use ChitChat\Http\Request;
+
+/** @var ChitChat\Config $config */
+$config = require dirname(__DIR__, 3) . '/bootstrap/http.php';
+
+Endpoint::run($config, static function () use ($config): ApiResult {
+    Request::requireMethod('GET');
+    $users = new UserRepository(Database::connect($config));
+    $user = SessionManager::currentUser($users);
+
+    return ApiResult::ok([
+        'csrf_token' => SessionManager::csrfToken(),
+        'user' => $user?->toArray(),
+    ]);
+});
