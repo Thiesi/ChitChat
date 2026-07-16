4e4192ca8ec6b17c4bb8f8b061f6f1af938f1700
Thiesi <Thiesi@users.noreply.github.com>
2026-07-16 23:34:38 +0200

    Add room detail endpoint

---
+++ public/api/v1/rooms/detail.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use ChitChat\Auth\SessionManager;
+use ChitChat\Auth\UserRepository;
+use ChitChat\Database;
+use ChitChat\Http\ApiResult;
+use ChitChat\Http\Endpoint;
+use ChitChat\Http\Request;
+use ChitChat\Room\RoomService;
+
+/** @var ChitChat\Config $config */
+$config = require dirname(__DIR__, 4) . '/bootstrap/http.php';
+
+Endpoint::run($config, static function () use ($config): ApiResult {
+    Request::requireMethod('GET');
+    $pdo = Database::connect($config);
+    $actor = SessionManager::requireUser(new UserRepository($pdo));
+    $room = (new RoomService($pdo))->get($actor, Request::queryInteger('room_id'));
+
+    return ApiResult::ok(['room' => $room->toArray()]);
+});
