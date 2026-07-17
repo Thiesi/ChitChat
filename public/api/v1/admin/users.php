3f16d189ddd6c1a74bab6364887f3e4b44ae4d9d
Thiesi <Thiesi@users.noreply.github.com>
2026-07-17 18:56:59 +0200

    Throttle administrator user search

--- public/api/v1/admin/users.php
+++ public/api/v1/admin/users.php
@@ -8,6 +8,7 @@ use ChitChat\Auth\UserRepository;
 use ChitChat\Database;
 use ChitChat\Http\ApiResult;
 use ChitChat\Http\Endpoint;
+use ChitChat\Http\RateLimiter;
 use ChitChat\Http\Request;
 
 /** @var ChitChat\Config $config */
@@ -17,6 +18,10 @@ Endpoint::run($config, static function () use ($config): ApiResult {
     Request::requireMethod('GET');
     $pdo = Database::connect($config);
     $actor = SessionManager::requireUser(new UserRepository($pdo));
+    (new RateLimiter($pdo, $config->rateLimits))->consume(
+        'admin_user_search',
+        'user:' . $actor->id,
+    );
     $users = (new AdminService($pdo))->listUsers(
         $actor,
         Request::optionalQueryString('search') ?? '',
