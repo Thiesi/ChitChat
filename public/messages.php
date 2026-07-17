16541cc0b31b7c437cc7edbe485a8fd46ee03393
Thiesi <Thiesi@users.noreply.github.com>
2026-07-17 08:44:14 +0200

    Load direct message blocking styles

--- public/messages.php
+++ public/messages.php
@@ -16,6 +16,7 @@ $appName = htmlspecialchars($config->applicationName, ENT_QUOTES | ENT_SUBSTITUT
   <link rel="stylesheet" href="/assets/css/app.css">
   <link rel="stylesheet" href="/assets/css/components.css">
   <link rel="stylesheet" href="/assets/css/messages.css">
+  <link rel="stylesheet" href="/assets/css/messages-blocking.css">
 </head>
 <body>
   <div id="messages-loading" class="app-loading" role="status">Loading messages…</div>
