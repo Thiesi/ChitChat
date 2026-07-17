0538775652eab03dee1603d2ecd6b76f92ee6e11
Thiesi <Thiesi@users.noreply.github.com>
2026-07-17 10:13:29 +0200

    Add direct message mutation metadata endpoint

---
+++ public/api/v1/direct-messages/message-mutations.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use ChitChat\Auth\SessionManager;
+use ChitChat\Auth\UserRepository;
+use ChitChat\Database;
+use ChitChat\DirectMessage\DirectMessageMutationService;
+use ChitChat\Http\ApiResult;
+use ChitChat\Http\Endpoint;
+use ChitChat\Http\MessageIdList;
+use ChitChat\Http\Request;
+
+/** @var ChitChat\Config $config */
+$config = require dirname(__DIR__, 4) . '/bootstrap/http.php';
+
+Endpoint::run($config, static function () use ($config): ApiResult {
+    Request::requireMethod('GET');
+    $pdo = Database::connect($config);
+    $actor = SessionManager::requireUser(new UserRepository($pdo));
+
+    return ApiResult::ok([
+        'messages' => (new DirectMessageMutationService($pdo))->metadata(
+            $actor,
+            MessageIdList::fromQuery($_GET['message_ids'] ?? null),
+        ),
+    ]);
+});
