4e771c06b0217c3353a6b1c397f51a8087656e52
Thiesi <Thiesi@users.noreply.github.com>
2026-07-17 03:19:58 +0200

    set v1.0.0-rc.1 application default

--- src/Config.php
+++ src/Config.php
@@ -86,7 +86,7 @@ final readonly class Config
             environment: self::env('APP_ENV', 'production'),
             debug: self::envBool('APP_DEBUG', false),
             applicationName: self::env('APP_NAME', 'ChitChat'),
-            applicationVersion: self::env('APP_VERSION', '0.1.0-dev'),
+            applicationVersion: self::env('APP_VERSION', '1.0.0-rc.1'),
             databaseHost: self::env('DB_HOST', '127.0.0.1'),
             databasePort: self::envInt('DB_PORT', 5432),
             databaseName: self::env('DB_NAME', 'chitchat'),
