aeb4ee2a4e9a5133240ea65a7d6268ac5702931a
Thiesi <Thiesi@users.noreply.github.com>
2026-07-18 08:52:57 +0200

    Add privacy notification list endpoint

---
+++ public/api/v1/account/notifications/list.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use ChitChat\Account\PrivacyNotificationService;
+use ChitChat\Auth\SessionManager;
+use ChitChat\Auth\UserRepository;
+use ChitChat\Database;
+use ChitChat\Http\ApiResult;
+use ChitChat\Http\Endpoint;
+use ChitChat\Http\Request;
+
+/** @var ChitChat\Config $config */
+$config = require dirname(__DIR__, 5) . '/bootstrap/http.php';
+
+Endpoint::run($config, static function () use ($config): ApiResult {
+    Request::requireMethod('GET');
+    $pdo = Database::connect($config);
+    $actor = SessionManager::requireUser(new UserRepository($pdo));
+    $beforeId = Request::optionalQueryInteger('before_id');
+    $limit = Request::optionalQueryInteger('limit') ?? 50;
+
+    return ApiResult::ok(
+        (new PrivacyNotificationService($pdo))->timeline($actor, $beforeId, $limit),
+    );
+});
