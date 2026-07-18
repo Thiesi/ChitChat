0553bfca4ecdfbad12f26edcca1f88d296d089ec
Thiesi <Thiesi@users.noreply.github.com>
2026-07-18 09:09:22 +0200

    Reset notification references in mutation tests

--- tests/Integration/MessageMutationServiceTest.php
+++ tests/Integration/MessageMutationServiceTest.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 namespace ChitChat\Tests\Integration;
 
 use ChitChat\Auth\AuthService;
@@ -46,7 +45,7 @@ final class MessageMutationServiceTest extends DatabaseTestCase
 
         $messages = new MessageService($this->pdo);
         $message = $messages->send($author, $room->id, 'Original room message');
-        $this->pdo->exec('TRUNCATE realtime_events, audit_log RESTART IDENTITY');
+        $this->pdo->exec('TRUNCATE account_notifications, realtime_events, audit_log RESTART IDENTITY');
         $mutations = new RoomMessageMutationService($this->pdo);
 
         $edited = $mutations->edit($author, $message['id'], 'Edited room message', '127.0.0.2');
@@ -134,7 +133,7 @@ SQL)->fetchAll();
         $bob = $auth->register('Bob', 'another secure password', '127.0.0.2');
         $messages = new DirectMessageService($this->pdo);
         $message = $messages->send($alice, $bob->id, 'Original private message');
-        $this->pdo->exec('TRUNCATE realtime_events, audit_log RESTART IDENTITY');
+        $this->pdo->exec('TRUNCATE account_notifications, realtime_events, audit_log RESTART IDENTITY');
         $mutations = new DirectMessageMutationService($this->pdo);
 
         $edited = $mutations->edit($alice, $message['id'], 'Edited private message', '127.0.0.1');
